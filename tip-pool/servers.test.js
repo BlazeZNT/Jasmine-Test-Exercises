2f3176d710a2538e4dddb53501ef3333c43814c4
Zar Ni Tun <zarnitunkt@gmail.com>
2023-12-01 21:56:50 +0630

    Complete Step 1 Tip Pool

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,26 @@
-describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+describe("Servers test (with setup and tear-down)", function () {
+	beforeEach(function () {
+		// initialization logic
+		serverNameInput.value = "Alice";
+	});
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+	it("should add a new server to allServers on submitServerInfo()", function () {
+		submitServerInfo();
+		expect(Object.keys(allServers).length).toEqual(1);
+		expect(allServers["server" + serverId].serverName).toEqual("Alice");
+	});
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+	it("makes sure that the table data is correct", function () {
+		submitServerInfo();
+		updateServerTable();
+		expect(serverTbody.firstChild.firstChild.innerHTML).toEqual("Alice");
+	});
 
-  afterEach(function() {
-    // teardown logic
-  });
+	afterEach(function () {
+		// teardown logic
+		serverNameInput.value = "";
+		serverTbody.innerHTML = "";
+		allServers = {};
+		serverId = 0;
+	});
 });
